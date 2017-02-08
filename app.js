55882114546192c04d628364dcc97cd32e082b2c
nasko <adachenski@aol.com>
2017-02-08 00:00:10 -0800

    Initial skeleton commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+
+var port = 5000;
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/',function(req, res){
+    res.send("Hello Main");
+});
+
+app.get('/books',function(req, res){
+    res.send("Hello from books");
+});
+
+app.listen(port,function(req, res){
+    console.log('App is running on port: '+port);
+});
